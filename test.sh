6ca06f47bb0b87a8097b5ed6338d5b2daba2a2f0
Samuel Frunza-Hincu <frunza_samuel@yahoo.com>
2026-02-12 17:35:18 +0100

    added Unit testing in python

--- test.sh
+++ test.sh
@@ -4,9 +4,4 @@
 set -e
 
 docker build -t pythonunittests .
-
-if command -v docker-compose >/dev/null 2>&1; then
-  docker-compose run --rm main
-else
-  docker compose run --rm main
-fi
+docker compose run --rm main
